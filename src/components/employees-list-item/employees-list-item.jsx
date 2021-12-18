9905d3cfa0759e573474845624f5cb5120f9a6e4
Yauheni Karas <yauhenikaras@ester.co>
2021-12-18 13:22:46 +0300

    add: handke click on the cookies

--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -1,31 +1,73 @@
+import { Component } from "react";
 import "./employees-list-item.scss";
 
-const EmployeesListItem = ({ name, salary, increase }) => {
-  let classNames = "list-group-item d-flex justify-content-between";
-  if (increase) {
-    classNames += " increase";
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+      like: false,
+    };
+    // this.handlerCookie = this.handlerCookie.bind(this);
   }
 
-  return (
-    <li className={classNames}>
-      <span className="list-group-item-label">{name}</span>
-      <input
-        type="text"
-        className="list-group-item-input"
-        defaultValue={salary + "$"}
-      />
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm ">
-          <i className="fas fa-cookie"></i>
-        </button>
+  // handlerCookie() {
+  //   this.setState((pre) => ({
+  //     increase: !pre.increase,
+  //   }));
+  // }
 
-        <button type="button" className="btn-trash btn-sm ">
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
-};
+  handlerCookie = () => {
+    this.setState(({ increase }) => ({
+      increase: !increase,
+    }));
+  };
+
+  handlerLike = () => {
+    this.setState(({ like }) => ({
+      like: !like,
+    }));
+  };
+
+  render() {
+    const { name, salary } = this.props;
+    const { increase, like } = this.state;
+    let classNames = "list-group-item d-flex justify-content-between";
+    if (increase) {
+      classNames += " increase";
+    }
+
+    if (like) {
+      classNames += " like";
+    }
+
+    return (
+      <li className={classNames}>
+        <span className="list-group-item-label" onClick={this.handlerLike}>
+          {name}
+        </span>
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={salary + "$"}
+        />
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-cookie btn-sm "
+            onClick={this.handlerCookie}
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button type="button" className="btn-trash btn-sm ">
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default EmployeesListItem;
